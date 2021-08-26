e60d3a76a87fd10efb9b76f55c04945210b4338c
Mack Maier <hmaiermack@gmail.com>
2021-08-26 09:53:29 -0700

    covid guidelines update

--- pages/covid.js
+++ pages/covid.js
@@ -7,7 +7,7 @@ const covid = () => {
         <Layout>
             <div className="container mt-52 text-center mx-auto">
                 <h1 className="text-vacblue text-4xl font-extrabold tracking-tighter uppercase leading-8 mb-2">Current Covid-19 Guidelines</h1>
-                <h2 className="text-gray-700 text-xl font-bold tracking-tighter uppercase mb-2">Updated: 7/6/2021
+                <h2 className="text-gray-700 text-xl font-bold tracking-tighter uppercase mb-2">Updated: 8/23/2021
                     <span className=" font-bold w-full block uppercase text-center leading-snug text-base mt-4">New Hours</span>
                     <span className=" font-bold w-full block uppercase text-center leading-snug text-base">M-F 4:45 AM - 9:00pm</span>
                     <span className=" font-bold w-full block uppercase text-center leading-snug text-base">Sat 8:00 AM - 6:00 PM</span>
@@ -21,20 +21,19 @@ const covid = () => {
                     <span className="ml-8 mb-4 font-bold w-full block">Scheduling is not required before visiting.</span>
                     <span className="ml-8 mb-4 font-bold w-full block">ALL AGES WELCOME!</span>
                     </p>
-                    <h2 className="text-vacblue text-xl font-extrabold text-center tracking-tighter uppercase leading-8 mb-8">
-                        Reserving a place prior to use is not required for the following:
-                    </h2>
 
                     <div className="mb-12">
                         <h3 className="text-vacblue text-lg font-bold tracking-tighter uppercase leading-8">
                         VAC Mask Policy
                         </h3>
                         <p className="max-w-prose text-gray-700 leading-relaxed ml-8">
-                        Fully vaccinated VAC members / users will not be required to wear face masks while using the facilities. WA Gov. 
-                        Inslee still requires that unvaccinated people wear face masks while indoors. If you are not vaccinated, please continue 
-                        to wear a face mask while using the VAC. Fully vaccinated members / users can choose to wear a mask or not and do whatever 
-                        they are most comfortable with.
+                        Due to the recent announcement by WA Gov. Inslee, face masks will be required again in indoor spaces beginning Monday 8/23/21 regardless of vaccination status. 
+                        Please review the following list that details which areas of the club will NOT require face masks. Face Masks ARE required for any indoor areas not in the following list:
                         </p>
+                        <span className="ml-8 mb-4 font-bold w-full block">While in the Pool water</span>
+                        <span className="ml-8 mb-4 font-bold w-full block">While in any room by yourself. If you have a room to yourself a mask is not required.</span>
+                        <span className="ml-8 mb-4 font-bold w-full block">While in the Training Center room with both garage doors open. This is the new room with the two large garage doors. 
+                        If both garage doors are open this is considered an outdoor space and face masks will NOT be required.</span>
                     </div>
                 </div>
             </div>
